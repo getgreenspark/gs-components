342e7796cfc7fef47be0150f8fa98d57be9a7524
Tamas Gerenyi <getamas04@gmail.com>
2025-03-05 15:32:32 +0100

    fix package entry point

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [vue(), dts({ outDir: 'dist', insertTypesEntry: true })],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  build: {
-    lib: {
-      entry: 'src/index.ts',
-      name: 'GsComponents',
-      fileName: (format) => `index.${format}.js`,
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
     },
   },
 })
