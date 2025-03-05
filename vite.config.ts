38ea978716c9a1391ac206fd97e962ee0dfc07f7
Tamas Gerenyi <getamas04@gmail.com>
2025-03-05 18:39:43 +0100

    add plugin entry point

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,47 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import typescript2 from 'rollup-plugin-typescript2'
 
 export default defineConfig({
-  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  plugins: [
+    vue(),
+    typescript2({
+      check: false,
+      include: ['src/components/*.vue', 'src/components/**/*.vue', 'src/*.ts', 'src/**/*.ts'],
+      tsconfigOverride: {
+        compilerOptions: {
+          outDir: 'dist',
+          sourceMap: true,
+          declaration: true,
+          declarationMap: true,
+        },
+      },
+      exclude: ['vite.config.ts', 'main.ts'],
+    }),
+  ],
+  build: {
+    cssCodeSplit: false,
+    lib: {
+      entry: './src/GsComponents.ts',
+      formats: ['es', 'cjs'],
+      name: 'ViewerPlugin',
+      fileName: (format) => `plugin.${format}.js`,
+    },
+
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        exports: 'named',
+        globals: {
+          Vue: 'Vue',
+        },
+      },
     },
   },
 })
