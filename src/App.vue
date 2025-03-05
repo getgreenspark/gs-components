bbb1f827a4cfc7eb71bd05c6706d328ded831ce3
Tamas Gerenyi <getamas04@gmail.com>
2025-03-05 10:24:17 +0100

    update home view

--- src/App.vue
+++ src/App.vue
@@ -1,231 +1,25 @@
-<script setup lang="ts">
-import GsButton from '@/components/GsButton.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <section>
-    <div><strong>Primary</strong></div>
-    <div><strong>Secondary</strong></div>
-    <div><strong>Success</strong></div>
-    <div><strong>Danger</strong></div>
-    <div><strong>Info</strong></div>
-    <div><strong>Link</strong></div>
-    <div>
-      <gs-button variant="primary" size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="secondary" size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="success" size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="danger" size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="info" size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="link" href="#"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="primary"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="secondary"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="success"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="danger"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="info"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="link" href="#"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="primary" size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="secondary" size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="success" size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="danger" size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="info" size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="link" href="#"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="primary" size="large" icon="mdi-plus"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="secondary" size="large" icon="mdi-plus"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="success" size="large" icon="mdi-plus"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="danger" size="large" icon="mdi-plus"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="info" size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="link" href="#"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="primary" size="large" full-width> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="secondary" size="large" full-width> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="success" size="large" full-width> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="danger" size="large" full-width> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="info" size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="link" href="#"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="primary" disabled size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="secondary" disabled size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="success" disabled size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="danger" disabled size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="info" disabled size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="link" href="#"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="primary" disabled> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="secondary" disabled> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="success" disabled> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="danger" disabled> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="info" disabled> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="link" href="#"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="primary" disabled size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="secondary" disabled size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="success" disabled size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="danger" disabled size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="info" disabled size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="link" href="#"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="primary" loading size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="secondary" loading size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="success" loading size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="danger" loading size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="info" loading size="small"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="link" href="#"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="primary" loading> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="secondary" loading> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="success" loading> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="danger" loading> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="info" loading> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="link" href="#"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="primary" loading size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="secondary" size="large" loading> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="success" size="large" loading> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="danger" size="large" loading> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="info" loading size="large"> Button </gs-button>
-    </div>
-    <div>
-      <gs-button variant="link" href="#"> Button </gs-button>
-    </div>
+  <section class="page">
+    <h1>Greenspark Component Library</h1>
   </section>
 </template>
 
 <style lang="scss">
 body {
-  padding: 20px;
+  font-family: 'Cabin', sans-serif;
 }
 
 section {
-  display: grid;
-  grid-template-columns: repeat(6, 1fr);
-}
-
-section > div {
   display: flex;
   align-items: center;
   justify-content: center;
-  padding: 20px;
-  border: 1px solid var(--ui-green);
+  height: 100vh;
+
+  h1 {
+    font-size: 45px;
+    font-weight: 700;
+  }
 }
 </style>
