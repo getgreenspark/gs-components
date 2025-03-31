f32e5aebe19b2010898128378382140952d026de
Donat Peter <34376640+donatPeter@users.noreply.github.com>
2025-03-31 17:08:07 +0200

    introduce info bar (#5)
    * introduce info bar
    
    * fixed based on review
    
    * changes based on PR
    
    * rename story file
    
    * rename story file
    
    * export component
    
    * rename file
    
    ---------
    
    Co-authored-by: donatpeter <donat@getgreenspark.com>

---
+++ src/stories/AlertBar.stories.ts
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import 'vuetify/styles'
+import '@/assets/style/fonts.css'
+import '@/assets/style/variables.css'
+import '@mdi/font/css/materialdesignicons.min.css'
+
+import GsAlertBar from '@/components/GsAlertBar.vue'
+
+const meta: Meta<typeof GsAlertBar> = {
+  title: 'Components/AlertBar',
+  component: GsAlertBar,
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      control: 'select',
+      options: ['info', 'success', 'error', 'warning'],
+      description: 'The variant style of the alert bar.',
+    },
+    fullWidth: {
+      control: 'boolean',
+      description: 'Makes the alert bar take the full width of its container.',
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof GsAlertBar>
+
+const message = 'Soufflé marshmallow chocolate cake marzipan cake soufflé cookie biscuit'
+export const Variants: Story = {
+  render: (args) => ({
+    components: { GsAlertBar },
+    setup() {
+      return { args, message }
+    },
+    template: `
+      <div class="d-flex ga-3 align-items-center flex-column">
+        <GsAlertBar type="success" :message="message" />
+        <GsAlertBar type="success" :message="message" title="Success Title" />
+        <GsAlertBar type="info" :message="message" />
+        <GsAlertBar type="info" :message="message" title="Info Title" />
+        <GsAlertBar type="warning" :message="message" />
+        <GsAlertBar type="warning" :message="message" title="Warning Title" />
+        <GsAlertBar type="error" :message="message" />
+        <GsAlertBar type="error" :message="message" title="Error Title" />
+      </div>
+    `,
+  }),
+}
