c7f2ecc99a73e577ef2976f2cc8173d8a9191fc9
donatpeter <donat@getgreenspark.com>
2025-04-09 14:54:21 +0200

    extend storybook

--- src/stories/ColorPicker.stories.ts
+++ src/stories/ColorPicker.stories.ts
@@ -6,6 +6,7 @@ import '@/assets/style/variables.css'
 import '@mdi/font/css/materialdesignicons.min.css'
 
 import GsColorPicker from '@/components/GsColorPicker.vue'
+import { ref } from 'vue'
 
 const meta: Meta<typeof GsColorPicker> = {
   title: 'Components/ColorPicker',
@@ -46,17 +47,21 @@ export const Default: Story = {
   }),
 }
 
+const selectedColor = ref<string>('#3b755f')
+const selectedColor2 = ref<string>('#3b755f')
 export const Variants: Story = {
   render: (args) => ({
     components: { GsColorPicker },
     setup() {
-      return { args }
+      return { args, selectedColor, selectedColor2 }
     },
     template: `
       <div class="d-flex ga-3 align-items-center flex-column">
         <GsColorPicker :label="'Active Color Picker Label'" :placeholder="'Placeholder'" />
         <GsColorPicker :label="'Disabled Color Picker Label'" :disabled="true" />
         <GsColorPicker :label="'Custom Color Mode Label'" :color-modes="['hsl']" />
+        <GsColorPicker v-model="selectedColor" :label="'Default Color Label'" :color-modes="['hsl']" />
+        <GsColorPicker v-model="selectedColor2" :label="'Default Color Label + Clearable'" clearable />
       </div>
     `,
   }),
