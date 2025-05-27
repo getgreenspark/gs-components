2f2a5ffb466528bdfc59761701af01ce4a5196b2
Tamás Gerényi <getamas04@gmail.com>
2025-05-27 15:31:19 +0200

    add input component (#16)
    * add input component
    
    * update input stories, add validation rules
    
    * fix message color
    
    * fix input error state story
    
    * add tolgee

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,6 +1,8 @@
 import { type App } from 'vue'
+import { VueTolgee } from '@tolgee/vue'
+import tolgee from '@/plugins/tolgee'
 import vuetify from './vuetify'
 
 export function registerPlugins(app: App): void {
-  app.use(vuetify)
+  app.use(VueTolgee, { tolgee }).use(vuetify)
 }
